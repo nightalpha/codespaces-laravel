220be92dc65e3aea798939332c56079ec092df84
nightalpha <132177678+nightalpha@users.noreply.github.com>
2025-05-18 06:06:22 +0000

    20250518

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+const host = process.env.VITE_HOST || '127.0.0.1';
+const port = parseInt(process.env.VITE_PORT || '5173');
 
 export default defineConfig({
-    plugins: [
+  server: {
+    host,   // ここを localhost から 0.0.0.0 に変更
+    port,        // ポート番号を固定（変更可）
+    strictPort: true,  // ポートが既に使われていたらエラーにする
+    origin: process.env.CODESPACES
+        ? `https://${process.env.CODESPACE_NAME}-${port}.app.github.dev`
+        : undefined,
+},
+      plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });
